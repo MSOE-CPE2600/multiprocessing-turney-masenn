105ec0d258f7cf1635feb35cf06ce40511f25e42
Matthew <sennm@msoe.edu>
2024-11-25 20:37:01 -0600

    intermitten

--- mandel.c
+++ mandel.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include "jpegrw.h"
 
 // local routines
@@ -17,6 +18,7 @@ static int iterations_at_point( double x, double y, int max );
 static void compute_image( imgRawImage *img, double xmin, double xmax,
 									double ymin, double ymax, int max );
 static void show_help();
+static void generate_frame(int width, int height, int xcenter, int ycenter, int xscale, int yscale, char* outfile, int max);
 
 
 int main( int argc, char *argv[] )
@@ -25,7 +27,6 @@ int main( int argc, char *argv[] )
 
 	// These are the default configuration values used
 	// if no command line arguments are given.
-	const char *outfile = "mandel.jpg";
 	double xcenter = 0;
 	double ycenter = 0;
 	double xscale = 4;
@@ -33,11 +34,13 @@ int main( int argc, char *argv[] )
 	int    image_width = 1000;
 	int    image_height = 1000;
 	int    max = 1000;
+	int    nprocs = 1;
+	int    total_images = 50;
 
 	// For each command line argument given,
 	// override the appropriate configuration value.
 
-	while((c = getopt(argc,argv,"x:y:s:W:H:m:o:h"))!=-1) {
+	while((c = getopt(argc,argv,"x:y:s:W:H:m:o:h:n:p"))!=-1) {
 		switch(c) 
 		{
 			case 'x':
@@ -59,23 +62,113 @@ int main( int argc, char *argv[] )
 				max = atoi(optarg);
 				break;
 			case 'o':
-				outfile = optarg;
+				// outfile = optarg;
+				printf(" ");
 				break;
 			case 'h':
 				show_help();
 				exit(1);
 				break;
+			case 'n':
+				printf("%s",optarg);
+				nprocs = atoi(optarg);
+				break;
 		}
 	}
+	printf("nprocs%d\n",nprocs);
 
 	// Calculate y scale based on x scale (settable) and image sizes in X and Y (settable)
-	yscale = xscale / image_width * image_height;
+	// yscale = xscale / image_width * image_height;
 
 	// Display the configuration of the image.
-	printf("mandel: x=%lf y=%lf xscale=%lf yscale=%1f max=%d outfile=%s\n",xcenter,ycenter,xscale,yscale,max,outfile);
+	// printf("mandel: x=%lf y=%lf xscale=%lf yscale=%1f max=%d outfile=%s\n",xcenter,ycenter,xscale,yscale,max,outfile);
 
-	// Create a raw image of the appropriate size.
-	imgRawImage* img = initRawImage(image_width,image_height);
+	// for (int img = 0; img < total_images; img++) {
+    //     if (img % nprocs == 0 && img > 0) {
+    //         // Parent process waits for all child processes to finish before starting next batch
+    //         for (int p = 0; p < nprocs; p++) {
+    //             wait(NULL);
+    //         }
+    //     }
+
+    //     pid_t pid = fork();
+    //     if (pid == 0) { // Child process
+	// 		// xscale = 4 - img; // Example: Decrement `xscale` with each image
+	// 		// yscale = xscale / image_width * image_height; // Recalculate `yscale` proportionally
+
+	// 		xcenter = -0.5 + 0.1 * img; // Move the center
+    // 		ycenter = -0.5 + 0.05 * img; // Adjust center vertically
+
+    //         // double xscale = initial_xscale - img * xscale_decrement; // Vary xscale
+    //         // double yscale = xscale / image_width * image_height; // Adjust yscale proportionally
+    //         char outfile[256];
+    //         snprintf(outfile, sizeof(outfile), "madel%d.jpg", img);
+
+    //         printf("Generating image %d: xscale=%lf yscale=%lf outfile=%s\n",
+    //                img, xscale, yscale, outfile);
+
+    //         generate_frame(image_width, image_height, xcenter, ycenter, xscale, yscale, outfile, max);
+    //         exit(0); // Ensure child process exits after generating the image
+    //     }
+    // }
+
+    // // Wait for the final batch of processes
+    // for (int img = total_images - nprocs; img < total_images; img++) {
+    //     wait(NULL);
+    // }
+
+    // printf("All images generated successfully.\n");
+
+
+
+	yscale = xscale * (double)image_height / image_width;
+
+	int frames_per_proc = total_images / nprocs; // Frames assigned to each process
+    int remaining_frames = total_images % nprocs; // Extra frames if not divisible evenly
+
+    for (int proc = 0; proc < nprocs; proc++) {
+        pid_t pid = fork();
+
+        if (pid == 0) { // Child process
+            int start_frame = proc * frames_per_proc;
+            int end_frame = start_frame + frames_per_proc;
+
+            // Assign remaining frames to the last process
+            if (proc == nprocs - 1) {
+                end_frame += remaining_frames;
+            }
+
+            printf("Process %d: Handling frames %d to %d\n", proc, start_frame, end_frame - 1);
+
+            for (int img = start_frame; img < end_frame; img++) {
+                int current_xscale = xscale - img; // Example: Decrease xscale per frame
+                int current_yscale = current_xscale / image_width * image_height; // Recalculate yscale
+
+                char outfile[256];
+                snprintf(outfile, sizeof(outfile), "madel%d.jpg", img);
+
+                printf("Process %d generating image %d: xscale=%d yscale=%d outfile=%s\n",
+                       proc, img, current_xscale, current_yscale, outfile);
+
+                generate_frame(image_width, image_height, xcenter, ycenter, current_xscale, current_yscale, outfile, max);
+            }
+
+            exit(0); // Ensure child process exits after finishing its frames
+        }
+    }
+
+    // Parent process waits for all children to finish
+    for (int proc = 0; proc < nprocs; proc++) {
+        wait(NULL);
+    }
+
+    printf("All images generated successfully.\n");
+    return 0;
+}
+
+// Function to generate a Mandelbrot image
+void generate_frame(int width, int height, int xcenter, int ycenter, int xscale, int yscale, char* outfile, int max){
+	imgRawImage* img = initRawImage(width,height);
 
 	// Fill it with a black
 	setImageCOLOR(img,0);
@@ -89,7 +182,6 @@ int main( int argc, char *argv[] )
 	// free the mallocs
 	freeRawImage(img);
 
-	return 0;
 }
 
 
@@ -160,7 +252,7 @@ Modify this function to make more interesting colors.
 */
 int iteration_to_color( int iters, int max )
 {
-	int color = 0xFFFFFF*iters/(double)max;
+	int color = 0xF0F0FF*iters/(double)max;
 	return color;
 }
 
